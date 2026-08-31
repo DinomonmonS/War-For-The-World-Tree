2361453c2956c8d42c225d953d028b8dbef00984
Zachary Colville <9955076@schoolsnet.act.edu.au>
2026-08-31 10:25:12 +1000

    blep

--- Characters/movement.gd
+++ Characters/movement.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 	if (velocity.x > 1 || velocity.x < 1):
 		sprite_2d.animation = "Run"
 	else:
-		sprite_2d.animation = "default"
+		sprite_2d.animation = "Idle"
 
 	# Add the gravity.
 	if not is_on_floor():
