f7e7ef83bea16858a7081c6d35f83febf47b6913
Zachary Colville <9955076@schoolsnet.act.edu.au>
2026-09-16 14:56:42 +1000

    all done :3

---
+++ Characters/Controller2.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
+var current_buttons = {}
+var previous_buttons = {}
+@onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var device_id: int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func get_horizontal() -> void:
+	var direction = 0.0
+	#if Input.is_joy_button_pressed(device_id, JOY_BUTTON_DPAD_LEFT):
+	if Input.is_action_just_pressed("Left P1") and device_id == 0:
+		direction = -1.0
+	#if Input.is_joy_button_pressed(device_id, JOY_BUTTON_DPAD_RIGHT):
+	if Input.is_action_just_pressed("Right P1") and device_id == 0:
+		direction = 1.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#if (previous_buttons == current_buttons):
+		#pass
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		sprite_2d.animation = "Jump"
+	get_horizontal()
+
+func button_pressed(button) -> bool:
+	return current_buttons.get(button, false)
--- Characters/movement.gd
+++ Characters/movement.gd
@@ -1,43 +1,43 @@
-extends CharacterBody2D
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-var array = [Input.get_connected_joypads()]
-@onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
-func _physics_process(delta: float) -> void:
-	var isleft =  velocity.x < 0
-	sprite_2d.flip_h = isleft
-	if (velocity.x > 1 || velocity.x < 1):
-		sprite_2d.animation = "Run"
-	else:
-		sprite_2d.animation = "Idle"
-
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		sprite_2d.animation = "Jump"
-	if (array[0] == 0): #Check if player is player 1
-		if Input.is_action_just_pressed("Jump P1") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-		var direction := Input.get_axis("Left P1", "Right P1")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	if (array[1] == 1): #Check if player is player 2
-		if Input.is_action_just_pressed("Jump P2") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-				
-		var direction := Input.get_axis("Left P2", "Right P2")
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
-	
-	
-	
+#extends CharacterBody2D
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -600.0
+#var array = [Input.get_connected_joypads()]
+#@onready var sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+#
+#func _physics_process(delta: float) -> void:
+	#var isleft =  velocity.x < 0
+	#sprite_2d.flip_h = isleft
+	#if (velocity.x > 1 || velocity.x < 1):
+		#sprite_2d.animation = "Run"
+	#else:
+		#sprite_2d.animation = "Idle"
+#
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+		#sprite_2d.animation = "Jump"
+	#if (array[0] == 0): #Check if player is player 1
+		#if Input.is_action_just_pressed("Jump P1") and is_on_floor():
+			#velocity.y = JUMP_VELOCITY
+#
+	## Get the input direction and handle the movement/deceleration.
+		#var direction := Input.get_axis("Left P1", "Right P1")
+		#if direction:
+			#velocity.x = direction * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+	#
+	#if (array[1] == 1): #Check if player is player 2
+		#if Input.is_action_just_pressed("Jump P2") and is_on_floor():
+			#velocity.y = JUMP_VELOCITY
+				#
+		#var direction := Input.get_axis("Left P2", "Right P2")
+		#if direction:
+			#velocity.x = direction * SPEED
+		#else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+	#move_and_slide()
+	#
+	#
+	#
